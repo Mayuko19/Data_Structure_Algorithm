a03f44f0626080949084566b6e6107a40b1f7d67
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-09-22 14:07:01 +0900

    Create Bubble Sort.py

---
+++ Bubble Sort.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def bubble_sort(numbers: List[int]) -> List[int]:
+   """
+
+   :type numbers: object
+   """
+   len_numbers = len(numbers)
+
+   for i in range (len_numbers):
+       for j in range(len_numbers-1-i):
+           if numbers[j] > numbers[j + 1]:
+               numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+   return numbers
+
+
+if __name__ == '__main__':
+    import random
+    nums = [random.randint(1, 1000) for i in range(10)]
+    print(bubble_sort(nums))
